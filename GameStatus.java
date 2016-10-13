3ca6847b3a786e9e150bb5fe1dde744bcfb61414
edwlee <edwlee@ucalgary.ca>
2016-10-13 16:54:19 -0600

    Original Copy to be Refactored. First commit

---
+++ GameStatus.java
@@ -0,0 +1,12 @@
+/*
+ The sole purpose of this class to store global flags that indicate the current
+ status of the game.
+*/
+public class GameStatus
+{
+    public static boolean gameLost;
+    public static boolean gameWon;
+    public static boolean gameTie;
+    public static boolean debugModeOn;
+    public static boolean cheatModeOn;
+}
---
+++ SpaceSimulator.java
@@ -0,0 +1,8 @@
+public class SpaceSimulator
+{
+    public static void main (String [] args)
+    {
+    	Galaxy userInterface = new Galaxy();
+    	userInterface.startProcessingCommands();
+    }
+}
