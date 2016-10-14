52702e226875bb275b35dc4e5c3bba0c3d5fbd9b
edwlee <edwlee@ucalgary.ca>
2016-10-14 00:01:47 -0600

    REFACTORING 5 - Inline Class. Added test cases for Galaxy to check state of game when ended. Also added test case for GameManager. got rid of GameStatus class as it was only variable flags.

--- SpaceSimulator.java
+++
@@ -1,8 +0,0 @@
-public class SpaceSimulator
-{
-    public static void main (String [] args)
-    {
-    	GameManager userInterface = new GameManager();
-    	userInterface.startProcessingCommands();
-    }
-}
